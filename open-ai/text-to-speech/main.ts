51c1b4f28bd5252360f40243a22ce098326036d7
gomdopi <io.gomdopi@gmail.com>
2024-04-15 14:54:23 -0700

    update main.ts & .gitignore

--- open-ai/text-to-speech/main.ts
+++ open-ai/text-to-speech/main.ts
@@ -1,28 +1,32 @@
-import { promises } from "fs"
-import { readFileSync } from 'fs'
-import { join, resolve } from "path"
-import OpenAI from "openai"
+import { promises } from 'fs'
+import { resolve } from 'path'
+import OpenAI from 'openai'
+import { args, ModelOptions, VoiceOptions } from './args'
+
+type GenerateSpeechArgs = {
+  model: ModelOptions,
+  voice: VoiceOptions,
+  input: string,
+}
 
 const openai = new OpenAI();
 
-const speechFile = resolve("./speech.mp3");
+const speechFile = resolve('./speech.mp3');
 
 async function main() {
-  const input = readFileSync(join(__dirname, 'script.txt'), 'utf-8')
+  const scriptLength = args.input.length
+  if (scriptLength >= 4096) throw new Error(`Input is too long: ${scriptLength}`)
 
-  if (input.length >= 4096) throw new Error(`Input is too long: ${input.length}`)
-
-  console.log(input.length)
-  generateSpeech(input)
+  generateSpeech(args)
 }
 
-async function generateSpeech(input: string) {
+async function generateSpeech(args: GenerateSpeechArgs) {
   const mp3 = await openai.audio.speech.create({
-    model: "tts-1",
-    voice: "onyx",
-    input,
+    model: args.model,
+    voice: args.voice,
+    input: args.input,
   });
-  console.log(speechFile);
+
   const buffer = Buffer.from(await mp3.arrayBuffer());
   await promises.writeFile(speechFile, buffer);
 }
